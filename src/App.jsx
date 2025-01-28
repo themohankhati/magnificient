0176254f6a213f095493d821ac434c6a9d8cef6e
themohankhati <mohankc596@gmail.com>
2025-01-28 22:41:31 +0545

    file struc

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import React from "react";
+
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1 className="text-3xl font-bold underline flex justify-center">
+         Hello world!
+    </h1>    
+   </div>
+    </>
+  )
+}
+
+export default App
