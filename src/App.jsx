ca12fe663a903232251e97718bb29a8d979d4619
ManojPanta <pantamanoj08@gmail.com>
2025-04-07 16:15:41 +0545

    changes occurs

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,6 @@ import Destinations from "./components/homepage/Destinations";
 import ActivitiesSection from "./components/homepage/Activities";
 import ContactUs from "./pages/contactUs";
 import AnnapurnaBaseCamp from "./components/homepage/Trek/AnnapurnaBaseCamp";
-<<<<<<< HEAD
-import ChitwanNationalPark1 from "./components/homepage/tour/jungle-safari/chitwan-national-park/ChitwanNationalPark1";
-import ChitwanNationalPark2 from "./components/homepage/tour/jungle-safari/chitwan-national-park/ChitwanNationalPark2";
-import ChitwanNationalPark3 from "./components/homepage/tour/jungle-safari/chitwan-national-park/ChitwanNationalPark3";
-import ChitwanNationalPark4 from "./components/homepage/tour/jungle-safari/chitwan-national-park/ChitwanNationalPark4";
-import ChitwanNationalPark5 from "./components/homepage/tour/jungle-safari/chitwan-national-park/ChitwanNationalPark5";
-import BardiyaNationalPark1 from "./components/homepage/tour/jungle-safari/bardiya-national-park/BardiyaNationalPark1";
-import BardiyaNationalPark2 from "./components/homepage/tour/jungle-safari/bardiya-national-park/BardiyaNationalPark2";
-import BardiyaNationalPark3 from "./components/homepage/tour/jungle-safari/bardiya-national-park/BardiyaNationalPark3";
-import BardiyaNationalPark4 from "./components/homepage/tour/jungle-safari/bardiya-national-park/BardiyaNationalPark4";
-import Dashboard from "./components/homepage/AdminDashboard/Components/Dashboard";
-import VendorDetails from "./components/homepage/AdminDashboard/Components/VendorDetails";
-=======
->>>>>>> parent of 1abcaef (Chitwan and bardiya tours added)
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 
