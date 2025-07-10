f9c300fbd4dbd534ba6725dea2977e6aaff5f0d9
mohan.khati <mohankc596@gmail.com>
2025-07-10 22:43:51 +0545

    feat: destinations setup implementation

---
+++ src/services/destinationsService.js
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const baseUrl = "http://localhost:3000/";
+
+export const useGetDestinations = () => {
+  return useQuery({
+    queryKey: ["GET_DESTINATIONS"],
+    queryFn() {
+      return axios.get(`${baseUrl}api/destination`);
+    },
+    onSuccess(res) {
+      return res?.data;
+    },
+  });
+};
