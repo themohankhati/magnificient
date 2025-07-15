35f394cb8f888dc2dcf49888ddbf7ab4155407df
mohan.khati <mohankc596@gmail.com>
2025-07-15 23:33:00 +0545

    fix: prettierrc configuration and deployment fixes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import BardiyaNationalPark1 from "./components/tour/jungle-safari/bardiya-nation
 import BardiyaNationalPark2 from "./components/tour/jungle-safari/bardiya-national-park/BardiyaNationalPark2";
 import BardiyaNationalPark3 from "./components/tour/jungle-safari/bardiya-national-park/BardiyaNationalPark3";
 import BardiyaNationalPark4 from "./components/tour/jungle-safari/bardiya-national-park/BardiyaNationalPark4";
-import Dashboard from "./components/homepage/AdminDashboard/Components/Dashboard"
+import Dashboard from "./components/homepage/AdminDashboard/Components/Dashboard";
 import UserDetails from "./components/homepage/AdminDashboard/Components/UserDetails";
 import CustomerDetails from "./components/homepage/AdminDashboard/Components/CustomerDetails";
 import AuthPage from "./pages/AuthPage";
@@ -24,36 +24,33 @@ import PackageDetails from "./components/homepage/AdminDashboard/Components/Pack
 import ActivityOverviewPage from "./pages/ActivityOverviewPage";
 import MagnificentTourOverviewPage from "./pages/MagnificentTourOverviewPage";
 import TrekkingOverviewPage from "./pages/TrekkingOverviewPage";
-import {QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
-
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <>
-      
       <QueryClientProvider client={queryClient}>
-
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense>
-                <HomePage />
-              </Suspense>
-            }
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Suspense>
+                  <HomePage />
+                </Suspense>
+              }
             />
-          <Route
-            path="/destinations"
-            element={
-              <Suspense>
-                <Destinations />
-              </Suspense>
-            }
+            <Route
+              path="/destinations"
+              element={
+                <Suspense>
+                  <Destinations />
+                </Suspense>
+              }
             />
             <Route
               path="/activities"
@@ -62,215 +59,213 @@ function App() {
                   <ActivitiesSection />
                 </Suspense>
               }
-              />
-          <Route
-            path="/aboutus"
-            element={
-              <Suspense>
-                <AboutUs />
-              </Suspense>
-            }
             />
-            
             <Route
-            path="/annapurnabasecamp"
-            element={
-              <Suspense>
-                <AnnapurnaBaseCamp />
-              </Suspense>
-            }
-          />
-            
+              path="/aboutus"
+              element={
+                <Suspense>
+                  <AboutUs />
+                </Suspense>
+              }
+            />
+
             <Route
-            path="/chitwan-national-park1"
-            element={
-              <Suspense>
-                <ChitwanNationalPark1 />
-              </Suspense>
-            }
+              path="/annapurnabasecamp"
+              element={
+                <Suspense>
+                  <AnnapurnaBaseCamp />
+                </Suspense>
+              }
             />
-             <Route
-            path="/login"
-            element={
-              <Suspense>
-                <AuthPage />
-              </Suspense>
-            }
-            />
-               <Route
-            path="/register"
-            element={
-              <Suspense>
-                <AuthPage />
-              </Suspense>
-            }
-            />
-          <Route
-            path="/package"
-            element={
-              <Suspense>
-                <ChitwanNationalPark2 />
-              </Suspense>
-            }
-            />
-            
+
             <Route
-            path="/chitwan-national-park3"
-            element={
-              <Suspense>
-                <ChitwanNationalPark3 />
-              </Suspense>
-            }
+              path="/chitwan-national-park1"
+              element={
+                <Suspense>
+                  <ChitwanNationalPark1 />
+                </Suspense>
+              }
             />
-            
             <Route
-            path="/chitwan-national-park4"
-            element={
-              <Suspense>
-                <ChitwanNationalPark4 />
-              </Suspense>
-            }
-          />
-            
+              path="/login"
+              element={
+                <Suspense>
+                  <AuthPage />
+                </Suspense>
+              }
+            />
             <Route
-            path="/chitwan-national-park5"
-            element={
-              <Suspense>
-                <ChitwanNationalPark5 />
-              </Suspense>
-            }
+              path="/register"
+              element={
+                <Suspense>
+                  <AuthPage />
+                </Suspense>
+              }
             />
-            
             <Route
-            path="/bardiya-national-park1"
-            element={
-              <Suspense>
-                <BardiyaNationalPark1 />
-              </Suspense>
-            }
-          />
-            
+              path="/package"
+              element={
+                <Suspense>
+                  <ChitwanNationalPark2 />
+                </Suspense>
+              }
+            />
+
             <Route
-            path="/bardiya-national-park2"
-            element={
-              <Suspense>
-                <BardiyaNationalPark2 />
-              </Suspense>
-            }
+              path="/chitwan-national-park3"
+              element={
+                <Suspense>
+                  <ChitwanNationalPark3 />
+                </Suspense>
+              }
             />
-            
+
             <Route
-            path="/bardiya-national-park3"
-            element={
-              <Suspense>
-                <BardiyaNationalPark3 />
-              </Suspense>
-            }
+              path="/chitwan-national-park4"
+              element={
+                <Suspense>
+                  <ChitwanNationalPark4 />
+                </Suspense>
+              }
             />
-            
+
             <Route
-            path="/bardiya-national-park4"
-            element={
-              <Suspense>
-                <BardiyaNationalPark4 />
-              </Suspense>
-            }
+              path="/chitwan-national-park5"
+              element={
+                <Suspense>
+                  <ChitwanNationalPark5 />
+                </Suspense>
+              }
             />
-          <Route
-            path="/contactus"
-            element={
-              <Suspense>
-                <ContactUs />
-              </Suspense>
-            }
-          />
-           <Route
-            path="/dashboard"
-            element={
-              <Suspense>
-                <Dashboard />
-              </Suspense>
-            }
+
+            <Route
+              path="/bardiya-national-park1"
+              element={
+                <Suspense>
+                  <BardiyaNationalPark1 />
+                </Suspense>
+              }
             />
-             <Route
-            path="/login"
-            element={
-              <Suspense>
-                <AuthPage />
-              </Suspense>
-            }
+
+            <Route
+              path="/bardiya-national-park2"
+              element={
+                <Suspense>
+                  <BardiyaNationalPark2 />
+                </Suspense>
+              }
             />
-               <Route
-            path="/register"
-            element={
-              <Suspense>
-                <AuthPage />
-              </Suspense>
-            }
+
+            <Route
+              path="/bardiya-national-park3"
+              element={
+                <Suspense>
+                  <BardiyaNationalPark3 />
+                </Suspense>
+              }
             />
-        <Route
-            path="/dashboardpackage"
-            element={
-              <Suspense>
-                <PackageDetails />
-              </Suspense>
-            }
+
+            <Route
+              path="/bardiya-national-park4"
+              element={
+                <Suspense>
+                  <BardiyaNationalPark4 />
+                </Suspense>
+              }
             />
-           <Route
-            path="/userdetails"
-            element={
-              <Suspense>
-                <UserDetails />
-              </Suspense>
-            }
+            <Route
+              path="/contactus"
+              element={
+                <Suspense>
+                  <ContactUs />
+                </Suspense>
+              }
             />
-          <Route
-            path="/customerdetails"
-            element={
-              <Suspense>
-                <CustomerDetails />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/tour/:tourId"
-            element={
-              <Suspense>
-                <TourOverviewPage />
-              </Suspense>
-            }
+            <Route
+              path="/dashboard"
+              element={
+                <Suspense>
+                  <Dashboard />
+                </Suspense>
+              }
             />
-          <Route
-            path="/activities/:activityId"
-            element={
-              <Suspense>
-                <ActivityOverviewPage />
-              </Suspense>
-            }
+            <Route
+              path="/login"
+              element={
+                <Suspense>
+                  <AuthPage />
+                </Suspense>
+              }
             />
-          <Route
-            path="/tours/:tourId"
-            element={
-              <Suspense>
-                <MagnificentTourOverviewPage />
-              </Suspense>
-            }
+            <Route
+              path="/register"
+              element={
+                <Suspense>
+                  <AuthPage />
+                </Suspense>
+              }
             />
-          <Route
-            path="/trekking/:trekkingId"
-            element={
-              <Suspense>
-                <TrekkingOverviewPage />
-              </Suspense>
-            }
+            <Route
+              path="/dashboardpackage"
+              element={
+                <Suspense>
+                  <PackageDetails />
+                </Suspense>
+              }
             />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+            <Route
+              path="/userdetails"
+              element={
+                <Suspense>
+                  <UserDetails />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/customerdetails"
+              element={
+                <Suspense>
+                  <CustomerDetails />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/tour/:tourId"
+              element={
+                <Suspense>
+                  <TourOverviewPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/activities/:activityId"
+              element={
+                <Suspense>
+                  <ActivityOverviewPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/tours/:tourId"
+              element={
+                <Suspense>
+                  <MagnificentTourOverviewPage />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/trekking/:trekkingId"
+              element={
+                <Suspense>
+                  <TrekkingOverviewPage />
+                </Suspense>
+              }
+            />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </>
   );
 }
-
-
 
 export default App;
